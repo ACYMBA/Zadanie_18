b04249590106f68da1b9f34f0332dfa023fdf4ac
albert <albert.cymbaluk@gmail.com>
2017-09-26 22:02:37 +0200

    new with problem

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+module.exports = {
+        entry: [
+          'react-hot-loader/patch',
+          'webpack-dev-server/client?http://localhost:8080',
+          'webpack/hot/only-dev-server',
+          './client/index.js'
+        ],
+        output: {
+          filename: './bundle.js',
+          path: resolve(__dirname, 'public'),
+        }
+     module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                         }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+      }),
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+          sourceMap: false
+        })
+        ]
+
+};
